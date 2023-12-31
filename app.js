01b470eb60ced4a18bb8fdac7ebe84657eb1fea3
Ruhul <k.m.ruhulkuddus@gmail.com>
2023-12-31 12:40:33 +0600

    A redme file adde for discription writing

---
+++ app.js
@@ -0,0 +1,18 @@
+function paragraphChange(){
+  document.getElementById('demo4').innerHTML= 'thank you ruhul your dedication for this learning;'
+}
+
+//document.write('this is Ruhul sir');
+
+function getAlert(){
+  alert('Are you ok sir');
+}
+
+console.log(4+6);
+
+let rohim, korim, jamal;
+rohim = 32;
+korim = 23;
+jamal = rohim + korim;
+document.getElementById('demo5').innerHTML=`jamal total is=${jamal}`;
+
